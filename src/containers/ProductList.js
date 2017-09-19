0cd848fd0e6e1e9db463a4d6e3bd948622e8c506
Adrian Rodriguez <adrianwritenow@gmail.com>
2017-09-19 10:48:13 -0400

    additional changes cleaner code

--- src/containers/ProductList.js
+++ src/containers/ProductList.js
@@ -1,24 +1,19 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {connect} from 'react-redux'
 
 import Product from "../components/Product";
 
-class ProductList extends Component {
-
-    render() {
-        const {products} = this.props;
-        return (
-            <ul className="ProductList">
-            {products.map(product => (<Product product={product}/>))}
-            </ul>
-        );
-
+const ProductList = ({products})=> {
+  return (
+      <ul className="ProductList">
+      {products.map(product => (<Product product={product}/>))}
+      </ul>
+  );
+}
         // Create a dynamically populated list of `<Product />` components
         // Each `<Product />` component should have a single object from the `products` state property (array)
         // applied to the component as a `product` property
 
-    }
-}
 
 // Using the `mapStateToProps` function, filter the array stored in the
 // state `products` property based on 3 criterea:
@@ -37,7 +32,7 @@ const mapStateToProps = function(state) {
         products = state.products;
     }
 
-    return {products: products}
+    return {products}
 }
 
 export default connect(mapStateToProps)(ProductList);
